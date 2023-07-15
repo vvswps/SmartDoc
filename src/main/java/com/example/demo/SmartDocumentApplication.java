7dd61b57f65f55c04d018eb22d5a001f8f82b30e
vvswps <as904465@gmail.com>
2023-07-15 23:06:33 +0530

    testing env

--- src/main/java/com/example/demo/SmartDocumentApplication.java
+++ src/main/java/com/example/demo/SmartDocumentApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartDocumentApplication {
 
 	public static void main(String[] args) {
+		// access the environment variable ${AZURE_DB_URL} and print out its last 4
+		// characters
+		System.out.println("Look at me \n\n\n\n\n\n\n\n\n\n");
+		System.out.println(
+				"AZURE_DB_URL: " + System.getenv("AZURE_DB_URL").substring(System.getenv("AZURE_DB_URL").length() - 8));
+		System.out.println("\n\n\n\n\n\n\n\n\n\n Bye");
+
 		SpringApplication.run(SmartDocumentApplication.class, args);
 	}
 
